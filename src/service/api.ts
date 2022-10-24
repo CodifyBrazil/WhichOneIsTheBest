71db9c5ef18f63708083886b84d286256250a693
Rafael Guilber <rafaelguilberdosreis@hotmail.com>
2022-10-24 15:01:14 -0300

    remove db.json

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: ''
-})
+    baseURL: 'https://api.unsplash.com',
+    headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'eZ1DaybOSJy_D16nsZRlAGcrzALknB9emwaosvKYvDU'
+            }
+    })
 
 export const API = {
-
+    
 }
