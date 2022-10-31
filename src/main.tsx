a8ff3466e2f2aa6b5a2311ad7b4fc29d80160a16
Rafael Guilber <rafaelguilberdosreis@hotmail.com>
2022-10-31 16:24:09 -0300

    :lipstick: style global font

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import './style.css';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
