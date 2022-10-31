e59dc71dddd0b0b68a83a172debf20fade419061
Rafael Guilber <rafaelguilberdosreis@hotmail.com>
2022-10-31 09:06:45 -0300

    :heavy_plus_sign: add universal-cookie

--- src/utils/localStore.ts
+++ src/utils/localStore.ts
@@ -1,11 +1,28 @@
+import { cookieStorageManager } from "@chakra-ui/react";
+
 type LocalStorage = {
-    session?: string;
-    valuePhoto?: number
+    session: string;
+    valuePhoto?: number;
+    pagePhoto?: number;
 }
 
-export const isLocalStorage = ({session, valuePhoto}: LocalStorage) => {
+export const isLocalStorage = ({session, valuePhoto, pagePhoto}: LocalStorage) => {
+    if (sessionStorage.getItem('sessionUser')) {
+        sessionStorage.setItem('sessionUser', session);
+        return true;
+    }
+    else{
+        const randomSessionUser = Math.floor(Math.random() * 9999999999999).toString();
+        sessionStorage.setItem('sessionUser', randomSessionUser);
+        const cookieUsers = {
+            session: sessionStorage.getItem('sessionUser'),
+            valuePhoto,
+            pagePhoto,
+        }
+        setCookie
 
+    }
 }
 
 
-// salve page and numberPage in localStorage or cockie for after
+// salve page and numberPage in localStorage or coockie for after
